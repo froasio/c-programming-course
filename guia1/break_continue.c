c7ac5fcae06063e4cf0b1ff9525c3bf18abbaa55
Federico Roasio <federico.roasio@mercadolibre.com>
2020-04-29 19:58:00 -0300

    Adds more examples

--- guia1/break_continue.c
+++ guia1/break_continue.c
@@ -10,6 +10,32 @@ int main(void) {
 		}
 		printf("%lu\n", i);
 	}
+
+	for (i = 0; i < 10; i++) {
+		if (i == 4) {
+			continue;
+		}
+		printf("%lu\n", i);
+	}
+
+	i = 0;
+	while (i < 10) {
+		printf("%lu\n", i);
+		i++;
+		if (i == 4) {
+	    	break;
+		}
+	}
+
+	i = 0;
+	while (i < 10) {
+		printf("%lu\n", i);
+		i++;
+		if (i == 4) {
+	    	continue;
+		}
+	}
+
 	return 0;
 }
 
