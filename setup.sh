9f92122139cf75811261bcdbdd27a97880705f36
Federico Martin Roasio <federoasio@gmail.com>
2020-05-12 11:09:53 +0000

    Configures replit run

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Setting enviroment..."
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
@@ -10,3 +11,4 @@ mkdir /home/runner/.apt
 mkdir /home/runner/.apt/usr
 mkdir /home/runner/.apt/usr/bin
 ln -s /home/runner/c-programming-course/build.sh /home/runner/.apt/usr/bin/build
+echo "Done!"
