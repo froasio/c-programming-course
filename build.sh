bc0ef7cc3a2065181ca552c2741fedfbc5cdc7ca
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-09 22:05:39 +0000

    Removes bin files

--- build.sh
+++ build.sh
@@ -20,7 +20,3 @@ done
 if [ "$run" = "true" ]; then
   ./$file
 fi
-
-
-
-
--- setup.sh
+++ setup.sh
@@ -11,4 +11,8 @@ mkdir /home/runner/.apt
 mkdir /home/runner/.apt/usr
 mkdir /home/runner/.apt/usr/bin
 ln -s /home/runner/c-programming-course/build.sh /home/runner/.apt/usr/bin/build
+
+find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
+
 echo "Done!"
+
