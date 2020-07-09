1b233b119c1b52881985d174c033419910ffcd96
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-09 22:31:28 +0000

    Clean up

--- misc/replit/setup.sh
+++ misc/replit/setup.sh
@@ -6,11 +6,11 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 
-chmod +x build.sh
+chmod +x /home/runner/c-programming-course/misc/replit/build.sh
 mkdir -p /home/runner/.apt
 mkdir -p /home/runner/.apt/usr
 mkdir -p /home/runner/.apt/usr/bin
-ln -sf /home/runner/c-programming-course/build.sh /home/runner/.apt/usr/bin/build
+ln -sf /home/runner/c-programming-course/misc/replit/build.sh /home/runner/.apt/usr/bin/build
 
 find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
 
