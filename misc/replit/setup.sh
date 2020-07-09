d6e647d992ab4e40e903613e2bfd7cb1574555e8
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-09 22:39:14 +0000

    Adds template

--- misc/replit/setup.sh
+++ misc/replit/setup.sh
@@ -7,10 +7,12 @@ git config --global alias.st status
 git config --global alias.br branch
 
 chmod +x /home/runner/c-programming-course/misc/replit/build.sh
+chmod +x /home/runner/c-programming-course/misc/replit/template.sh
 mkdir -p /home/runner/.apt
 mkdir -p /home/runner/.apt/usr
 mkdir -p /home/runner/.apt/usr/bin
 ln -sf /home/runner/c-programming-course/misc/replit/build.sh /home/runner/.apt/usr/bin/build
+ln -sf /home/runner/c-programming-course/misc/replit/template.sh /home/runner/.apt/usr/bin/template
 
 find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
 
---
+++ misc/replit/template.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /home/runner/c-programming-course/misc/replit/template.c $1
