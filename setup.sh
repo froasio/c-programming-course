16df0b4aec84eafaa0076503d4e586cd879427a3
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-09 22:11:47 +0000

    Repl.it setup does not fail on retry

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ git config --global alias.st status
 git config --global alias.br branch
 
 chmod +x build.sh
-mkdir /home/runner/.apt
-mkdir /home/runner/.apt/usr
-mkdir /home/runner/.apt/usr/bin
-ln -s /home/runner/c-programming-course/build.sh /home/runner/.apt/usr/bin/build
+mkdir -p /home/runner/.apt
+mkdir -p /home/runner/.apt/usr
+mkdir -p /home/runner/.apt/usr/bin
+ln -sf /home/runner/c-programming-course/build.sh /home/runner/.apt/usr/bin/build
 
 find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
 
