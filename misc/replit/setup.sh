f4235d922779936b80ca66c41487e13691b17a22
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-10 19:42:04 +0000

    Updates setup

--- misc/replit/setup.sh
+++ misc/replit/setup.sh
@@ -2,23 +2,20 @@
 
 echo "Setting enviroment..."
 
-if [ -f ./misc/replit/.envs ]; then
-    export $(cat ./misc/replit/.envs  | grep -v '#' | awk '/=/ {print $1}')
-		echo ${REPLIT_PATH}
-fi
+export LOCAL_ROOT_PATH=/home/runner
 
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 
-chmod +x ${REPLIT_PATH}/c-programming-course/misc/replit/build.sh
-chmod +x ${REPLIT_PATH}/c-programming-course/misc/replit/template.sh
-mkdir -p ${REPLIT_PATH}/.apt
-mkdir -p ${REPLIT_PATH}/.apt/usr
-mkdir -p ${REPLIT_PATH}/.apt/usr/bin
-ln -sf ${REPLIT_PATH}/c-programming-course/misc/replit/build.sh ${REPLIT_PATH}/.apt/usr/bin/build
-ln -sf ${REPLIT_PATH}/c-programming-course/misc/replit/template.sh ${REPLIT_PATH}/.apt/usr/bin/template
+chmod +x ${LOCAL_ROOT_PATH}/c-programming-course/misc/replit/build.sh
+chmod +x ${LOCAL_ROOT_PATH}/c-programming-course/misc/replit/template.sh
+mkdir -p ${LOCAL_ROOT_PATH}/.apt
+mkdir -p ${LOCAL_ROOT_PATH}/.apt/usr
+mkdir -p ${LOCAL_ROOT_PATH}/.apt/usr/bin
+ln -sf ${LOCAL_ROOT_PATH}/c-programming-course/misc/replit/build.sh ${LOCAL_ROOT_PATH}/.apt/usr/bin/build
+ln -sf ${LOCAL_ROOT_PATH}/c-programming-course/misc/replit/template.sh ${LOCAL_ROOT_PATH}/.apt/usr/bin/template
 
 find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
 
