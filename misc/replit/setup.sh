cdc2658f265e79f82956f9571e1d8361b6aaf5f4
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-10 17:49:07 +0000

    Adds env variables for replit

--- misc/replit/setup.sh
+++ misc/replit/setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 echo "Setting enviroment..."
+
+if [ -f ./misc/replit/.envs ]; then
+    export $(cat ./misc/replit/.envs  | grep -v '#' | awk '/=/ {print $1}')
+		echo ${REPLIT_PATH}
+fi
+
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 
-chmod +x /home/runner/c-programming-course/misc/replit/build.sh
-mkdir -p /home/runner/.apt
-mkdir -p /home/runner/.apt/usr
-mkdir -p /home/runner/.apt/usr/bin
-ln -sf /home/runner/c-programming-course/misc/replit/build.sh /home/runner/.apt/usr/bin/build
+chmod +x ${REPLIT_PATH}/c-programming-course/misc/replit/build.sh
+chmod +x ${REPLIT_PATH}/c-programming-course/misc/replit/template.sh
+mkdir -p ${REPLIT_PATH}/.apt
+mkdir -p ${REPLIT_PATH}/.apt/usr
+mkdir -p ${REPLIT_PATH}/.apt/usr/bin
+ln -sf ${REPLIT_PATH}/c-programming-course/misc/replit/build.sh ${REPLIT_PATH}/.apt/usr/bin/build
+ln -sf ${REPLIT_PATH}/c-programming-course/misc/replit/template.sh ${REPLIT_PATH}/.apt/usr/bin/template
 
 find . -not -path '*/\.*' -type f -exec sh -c "file -i '{}' | grep -q 'charset=binary'" \; -print | xargs rm -f
 
---
+++ misc/replit/template.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ${REPLIT_PATH}/c-programming-course/misc/replit/template.c $1
