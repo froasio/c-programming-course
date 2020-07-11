7560aa1e8ea2a37ecbe3f49cf0845d5f376a1b24
Federico Martin Roasio <froasio@fi.uba.ar>
2020-07-11 21:47:53 +0000

    Adds function pointers exercises

---
+++ function_pointers/print.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PRINTERS 2
+#define MAX_USERS 3
+
+typedef struct {
+	size_t id;
+	size_t age;
+} user_t;
+
+void print_verbose(user_t *u);
+void print_csv(user_t *u);
+
+void (*printers[MAX_PRINTERS])(user_t *) = {
+	print_verbose,
+	print_csv
+};
+
+int main(void) {
+
+	user_t users[MAX_USERS] = {
+		{1,20},
+		{2,30},
+		{3,40}	
+	};
+
+	size_t i,j;
+
+	puts("----------");
+	for(j = 0; j < MAX_PRINTERS; j++) {
+		for(i = 0; i < MAX_USERS; i++) {
+			printers[j](&(users[i]));
+		}
+		puts("----------");
+	}
+
+	return EXIT_SUCCESS;
+}
+
+void print_verbose(user_t *u) {
+	printf("ID: %lu, AGE: %lu\n", u->id, u->age);
+}
+
+void print_csv(user_t *u){
+		printf("%lu, %lu\n", u->id, u->age);
+}
